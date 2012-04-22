37a70ca489f31d93016f70a74e124b05e19d7dd5
c9s <cornelius.howl@gmail.com>
2012-04-22 10:20:10 +0800

    Retab

--- src/AssetKit/AssetLoader.php
+++ src/AssetKit/AssetLoader.php
@@ -3,38 +3,38 @@ namespace AssetKit;
 
 class AssetLoader
 {
-	public $paths;
-	public $config;
+    public $paths;
+    public $config;
 
-	function __construct($config,$paths = array())
-	{
-		$this->config = $config;
-		$this->paths = $paths;
-	}
+    function __construct($config,$paths = array())
+    {
+        $this->config = $config;
+        $this->paths = $paths;
+    }
 
 
 
-	function load($name)
-	{
-		if( $this->config && $path = $this->config->getAssetPath($name) ) {
-			$manifestFile = $path . DIRECTORY_SEPARATOR . 'manifest.yml';
-			$m = new Asset( $manifestFile );
-			$m->config = $this->config;
-			$m->loader = $this;
-			return $m;
-		}
-		else {
-			foreach( $this->paths as $path ) {
-				$manifestFile = $path . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'manifest.yml';
-				if( file_exists($manifestFile) ) {
-					$m = new Asset( $manifestFile );
-					$m->config = $this->config;
-					$m->loader = $this;
-					return $m;
-				}
-			}
-		}
-	}
+    function load($name)
+    {
+        if( $this->config && $path = $this->config->getAssetPath($name) ) {
+            $manifestFile = $path . DIRECTORY_SEPARATOR . 'manifest.yml';
+            $m = new Asset( $manifestFile );
+            $m->config = $this->config;
+            $m->loader = $this;
+            return $m;
+        }
+        else {
+            foreach( $this->paths as $path ) {
+                $manifestFile = $path . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'manifest.yml';
+                if( file_exists($manifestFile) ) {
+                    $m = new Asset( $manifestFile );
+                    $m->config = $this->config;
+                    $m->loader = $this;
+                    return $m;
+                }
+            }
+        }
+    }
 
 }
 
