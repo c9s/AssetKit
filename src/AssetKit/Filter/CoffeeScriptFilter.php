54ee1e103bb7802051636d1f9bdcf57dfbc5851c
c9s <cornelius.howl@gmail.com>
2012-04-24 17:07:57 +0800

    test coffeescript filter

--- src/AssetKit/Filter/CoffeeScriptFilter.php
+++ src/AssetKit/Filter/CoffeeScriptFilter.php
@@ -1,26 +1,36 @@
 <?php
 namespace AssetKit\Filter;
+use Symfony\Component\Process\ProcessBuilder;
 
 class CoffeeScriptFilter
 {
     public $coffeescript;
     public $nodejs;
 
-    public function __construct($coffeescript, $nodejs = 'node' )
+    public function __construct($coffeescript, $nodejs = null )
     {
-        $this->nodejs = $nodejs;
         $this->coffeescript = $coffeescript;
+        if( $nodejs ) {
+            $this->nodejs = $nodejs;
+        }
     }
 
     public function filter($collection)
     {
-        $pb = new ProcessBuilder(array( $this->nodejs, $this->coffeescript ));
+        $input = $collection->getContent();
+
+        if( $this->nodejs ) {
+            $pb = new ProcessBuilder(array( $this->nodejs, $this->coffeescript ));
+        }
+        else {
+            $pb = new ProcessBuilder(array( $this->coffeescript ));
+        }
         // compile and print to stdout
         $pb->add( '-cp' );
+        $pb->add( '--stdio');
+        $pb->setInput($input);
 
-        $input = $collection->getContent();
         $proc = $pb->getProcess();
-        $proc->setInput($input);
         $code = $proc->run();
         $content = $proc->getOutput();
         $collection->setContent($content);
