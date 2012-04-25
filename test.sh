116ddbd5b34e94a048a528d86b924873f96a20e1
c9s <cornelius.howl@gmail.com>
2012-04-25 14:23:59 +0800

    Add command line test script.

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+php scripts/assetkit.php init --public public/assets
+php scripts/assetkit.php add assets/blueprint/manifest.yml
+php scripts/assetkit.php compile --as blueprint blueprint
