e81d00235fec48be2a6480dae9939f5c3e521edf
c9s <cornelius.howl@gmail.com>
2012-04-26 17:12:28 +0800

    extract rewrite method

--- src/AssetKit/Filter/CssRewriteFilter.php
+++ src/AssetKit/Filter/CssRewriteFilter.php
@@ -11,6 +11,36 @@ namespace AssetKit\Filter;
  */
 class CssRewriteFilter
 {
+
+
+    public function rewritePath($content,$urlBase,$dir)
+    {
+        return preg_replace_callback('#
+            url\( 
+                (\'|"|)
+                (?<url>.*?)
+                \1
+            \)
+            #xs', function($matches) use($urlBase,$dir) {
+                $url = $matches['url'];
+                // XXX: dirty, do not rewrite @import css syntax
+                if( preg_match('/\.css$/',$url) ) {
+                    return $matches[0];
+                }
+
+                if( '/' === $url[0] )
+                    return $matches[0];
+
+                // rewrite with public asset baseurl
+                while (0 === strpos($url, '../') && 2 <= substr_count($dir, '/')) {
+                    $dir = substr($dir, 0, strrpos(rtrim($dir, '/'), '/') + 1);
+                    $url = substr($url, 3);
+                }
+                // echo "Replacing " , $matches['url'] , " to " , '/' . $dir . '/' . $url , "\n";
+                return str_replace( $matches['url'], '/' . $dir . '/' . $url , $matches[0]);
+            }, $content );
+    }
+
     public function filter($collection)
     {
 
@@ -21,30 +51,7 @@ class CssRewriteFilter
         foreach( $paths as $path ) {
             $dir = dirname($path);
             $content = file_get_contents($path);
-            $content = preg_replace_callback('#
-                url\( 
-                    (\'|"|)
-                    (?<url>.*?)
-                    \1
-                \)
-                #xs', function($matches) use($urlBase,$dir) {
-                    $url = $matches['url'];
-                    // XXX: dirty, do not rewrite @import css syntax
-                    if( preg_match('/\.css$/',$url) ) {
-                        return $matches[0];
-                    }
-
-                    if( '/' === $url[0] )
-                        return $matches[0];
-
-                    // rewrite with public asset baseurl
-                    while (0 === strpos($url, '../') && 2 <= substr_count($dir, '/')) {
-                        $dir = substr($dir, 0, strrpos(rtrim($dir, '/'), '/') + 1);
-                        $url = substr($url, 3);
-                    }
-                    // echo "Replacing " , $matches['url'] , " to " , '/' . $dir . '/' . $url , "\n";
-                    return str_replace( $matches['url'], '/' . $dir . '/' . $url , $matches[0]);
-                }, $content );
+            $content = $this->rewritePath($content,$urlBase,$dir);
             $contents .= $content;
         }
         $collection->setContent($contents);
