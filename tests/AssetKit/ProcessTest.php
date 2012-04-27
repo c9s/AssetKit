ff6a253a64ecac21943e8af7306def16d1a3ff5d
c9s <cornelius.howl@gmail.com>
2012-04-27 15:27:55 +0800

    Add a simple process test

---
+++ tests/AssetKit/ProcessTest.php
@@ -0,0 +1,13 @@
+<?php
+use AssetKit\Process;
+
+class ProcessTest extends PHPUnit_Framework_TestCase
+{
+    function test()
+    {
+        $proc = new Process(array('ls','-1'));
+        $return = $proc->run();
+        is( 0, $return );
+    }
+}
+
