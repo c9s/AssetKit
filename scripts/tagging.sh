8fbe8346c27b1a386abc7bce522f38b30d8291d3
c9s <cornelius.howl@gmail.com>
2012-05-12 14:53:32 +0800

    Improve tagging script

--- scripts/tagging.sh
+++ scripts/tagging.sh
@@ -2,4 +2,6 @@
 VERSION=$(cat package.ini | grep "^version" | perl -pe 's/version\s*=\s*//')
 DATE=$(date)
 git tag $VERSION -f -m "Release $VERSION at $DATE"
-git push origin --tags
+git remote | while read remote ; do
+    git push --quiet $remote --tags
+done
