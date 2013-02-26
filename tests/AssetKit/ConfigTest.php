a6c6cbcefe61932a544183eb9f4efc7b748c6b79
c9s <cornelius.howl@gmail.com>
2013-02-26 23:10:04 +0800

    Add ConfigTest

---
+++ tests/AssetKit/ConfigTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class ConfigTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $config = new AssetKit\Config("tests/assetkit.config");
+        ok($config);
+    }
+}
+
