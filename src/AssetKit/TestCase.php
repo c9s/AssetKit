482ccc718a13666153cf84d2f1e45f89c190fa2c
c9s <cornelius.howl@gmail.com>
2013-02-27 23:20:51 +0800

    Move testing methods to AssetKit/TestCase

--- src/AssetKit/TestCase.php
+++ src/AssetKit/TestCase.php
@@ -5,5 +5,42 @@ use PHPUnit_Framework_TestCase;
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
 
+    public $config;
+
+    public function getConfigFile()
+    {
+        $filename = str_replace('\\', '_', get_class($this));
+        return "tests/$filename.php";
+    }
+
+    public function getConfig()
+    {
+        $config = $this->config;
+        ok($config, 'asset config object');
+        return $config;
+    }
+
+    public function getLoader()
+    {
+        return new \AssetKit\AssetLoader($this->getConfig());
+    }
+
+    public function setUp()
+    {
+        $configFile = $this->getConfigFile();
+        if(file_exists($configFile)) {
+            unlink($configFile);
+        }
+        $this->config = new \AssetKit\AssetConfig($configFile);
+    }
+
+    public function tearDown()
+    {
+        $configFile = $this->getConfigFile();
+        if(file_exists($configFile)) {
+            unlink($configFile);
+        }
+    }
+
 }
 
