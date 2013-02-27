4bc9fa7409363c14b254429a7b82b299c25e2145
c9s <cornelius.howl@gmail.com>
2013-02-27 16:39:07 +0800

    inclue assetkit.php

---
+++ .assetkit.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'baseDir' => 'public/assets',
+  'baseUrl' => '/assets',
+  'dirs' => 
+  array (
+    0 => 'assets',
+    1 => 'assets',
+  ),
+  'assets' => 
+  array (
+  ),
+);
