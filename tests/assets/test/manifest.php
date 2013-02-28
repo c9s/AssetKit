ba8e9b20fdf5ab738a2ad873178ec5e9fc01a975
c9s <cornelius.howl@gmail.com>
2013-02-28 18:27:21 +0800

    Add compiled manifest.php

---
+++ tests/assets/test/manifest.php
@@ -0,0 +1,37 @@
+<?php return array (
+  'collections' => 
+  array (
+    0 => 
+    array (
+      'stylesheet' => 1,
+      'files' => 
+      array (
+        0 => 'css/main.css',
+      ),
+    ),
+    1 => 
+    array (
+      'filters' => 
+      array (
+        0 => 'sass',
+      ),
+      'stylesheet' => 1,
+      'files' => 
+      array (
+        0 => 'sass-test.sass',
+      ),
+    ),
+    2 => 
+    array (
+      'filters' => 
+      array (
+        0 => 'coffeescript',
+      ),
+      'javascript' => 1,
+      'files' => 
+      array (
+        0 => 'test.coffee',
+      ),
+    ),
+  ),
+);
