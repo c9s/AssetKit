adcc8b7072fbc548fcd93e9e5206676dafef383b
c9s <cornelius.howl@gmail.com>
2013-03-02 22:42:48 +0800

    Add .travis ci config

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# phpunit tests
-php scripts/assetkit.php init --public public
-php scripts/assetkit.php add assets/blueprint/manifest.yml
-php scripts/assetkit.php add assets/jquery/manifest.yml
-php scripts/assetkit.php add assets/jquery-ui/manifest.yml
-php scripts/assetkit.php compile --as blueprint blueprint
