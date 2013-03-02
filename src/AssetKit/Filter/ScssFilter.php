f69878fd859c33e6f640896944eb92b9bf6b7e2d
c9s <cornelius.howl@gmail.com>
2013-03-02 18:57:48 +0800

    Fix scss filter

--- src/AssetKit/Filter/ScssFilter.php
+++ src/AssetKit/Filter/ScssFilter.php
@@ -5,11 +5,11 @@ use RuntimeException;
 
 class ScssFilter 
 {
-    public $sass;
+    public $scss;
 
-    public function __construct($sass = 'sass')
+    public function __construct($sass = 'scss')
     {
-        $this->sass = $sass;
+        $this->scss = $scss;
     }
 
     public function filter($collection)
@@ -17,18 +17,16 @@ class ScssFilter
         if( ! $collection->isStylesheet )
             return;
 
-        $input = $collection->getContent();
-        $proc = new Process(array( $this->sass ));
-
+        $proc = new Process(array( $this->scss ));
+        $filepaths = $collection->getSourcePaths(true);
+        foreach($filepaths as $filepath) {
+            $proc->arg($filepath);
+        }
         // compile and print to stdout
-        $proc->arg( '-s' )->arg('--scss')->input($input);
         $code = $proc->run();
-
         if( $code != 0 )
             throw new RuntimeException("process error: $code");
-        
-        $content = $proc->getOutput();
-        $collection->setContent($content);
+        $collection->setContent($proc->getOutput());
     }
 
 }
