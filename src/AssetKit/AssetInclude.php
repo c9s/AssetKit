f0ce24a8ed01df56db6e8a73d7c6a05d9ee63a8f
c9s <cornelius.howl@gmail.com>
2013-03-02 19:06:28 +0800

    Add AssetInclude

---
+++ src/AssetKit/AssetInclude.php
@@ -0,0 +1,68 @@
+<?php
+namespace AssetKit;
+use Exception;
+
+/**
+ *
+ * $render = new AssetInclude;
+ * $render->render( $manifest );
+ */
+class AssetInclude
+{
+
+    /**
+     * @param string $url
+     * @param array $attributes
+     */
+    public function getJavascriptTag($url,$attributes = array())
+    {
+        $html = '<script type="text/javascript" ';
+        $html .= ' src="' . $url . '" ';
+        foreach( $attributes as $name => $value ) {
+            $html .= ' ' . $name . '="' . $value . '"'; 
+        }
+        $html .= '> </script>' . "\n";
+        return $html;
+    }
+
+
+    /**
+     * @param string $url
+     * @param array $attributes
+     */
+    public function getStylesheetTag($url,$attributes = array())
+    {
+        $html = '<link rel="stylesheet" type="text/css" ';
+        $html .= ' href="' . $url . '"' ;
+        foreach( $attributes as $name => $value ) {
+            $html .= ' ' . $name . '="' . $value . '"'; 
+        }
+        $html .= '/>' . "\n";
+        return $html;
+    }
+
+
+    /**
+     * @param array $manifest
+     */
+    public function render($manifest)
+    {
+        $html = '';
+        // render stylesheets first.
+        foreach( $manifest['css_url'] as $stylesheet ) {
+            /*
+            $stylesheet['url'];
+            $stylesheet['path'];
+            $stylesheet['attrs'];
+            */
+            $html .= $this->getStylesheetTag( $stylesheet['url'] , $stylesheet['attrs'] );
+        }
+
+        foreach( $manifest['js_url'] as $javascript ) {
+            $html .= $this->getJavascriptTag( $javascript['url'] , $javascript['attrs'] );
+        }
+        return $html;
+    }
+}
+
+
