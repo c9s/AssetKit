a2366a4f1a712d47d7efedd6a06cbfa64513c195
c9s <cornelius.howl@gmail.com>
2013-03-03 12:34:42 +0800

    some fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
-require 'PHPUnit/TestMore.php';
-require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
 define('ROOT', dirname(__DIR__));
+require 'PHPUnit/TestMore.php';
+require ROOT . '/vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
+apc_clear_cache();
 $classLoader = new \Universal\ClassLoader\BasePathClassLoader(array(
     ROOT . '/src', 
     ROOT . '/vendor/pear',
 ));
-// $classLoader->useIncludePath(true);
+$classLoader->useIncludePath(true);
 $classLoader->register();
