d2f6c4165376e9e1d4ce15e5049950e47bba4695
c9s <cornelius.howl@gmail.com>
2013-03-11 19:08:29 +0800

    the scss filter can simply inherit from sassfilter

--- src/AssetToolkit/Filter/ScssFilter.php
+++ src/AssetToolkit/Filter/ScssFilter.php
@@ -4,44 +4,14 @@ use AssetToolkit\Process;
 use AssetToolkit\Utils;
 use RuntimeException;
 
-class ScssFilter 
+class ScssFilter extends SassFilter
 {
-    public $scss;
-    public $fromFile = true;
-
-    public function __construct($scss = null)
+    public function __construct($bin = null)
     {
-        if ( $scss ) {
-            $this->scss = $scss;
+        if ( $bin ) {
+            $this->bin = $bin;
         } else {
-            $this->scss = Utils::findbin('scss');
+            $this->bin = Utils::findbin('scss');
         }
     }
-
-    public function filter($collection)
-    {
-        if( ! $collection->isStylesheet )
-            return;
-
-        $proc = new Process(array( $this->scss ));
-        $proc->arg('--compass');
-
-        if($this->fromFile) {
-            $filepaths = $collection->getSourcePaths(true);
-            foreach($filepaths as $filepath) {
-                $proc->arg($filepath);
-            }
-        } else {
-            $content = $collection->getContent();
-            $proc->arg('-s')->input( $collection->getContent() );
-        }
-        // compile and print to stdout
-        $code = $proc->run();
-        if ( $code != 0 ) {
-            throw new RuntimeException("ScssFilter failure: $code");
-        }
-        $collection->setContent($proc->getOutput());
-    }
-
 }
-
