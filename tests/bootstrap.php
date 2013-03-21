96be4f94cd154811c6fa912065101ecf1bf75ea6
c9s <cornelius.howl@gmail.com>
2013-03-21 16:46:55 +0800

    Fix test bootstrap script

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,9 @@
 <?php
 require 'vendor/autoload.php';
-require 'PHPUnit/TestMore.php';
 
 if( extension_loaded('apc') ) {
     apc_clear_cache();
 }
 if (extension_loaded('xhprof') ) {
     ini_set('xhprof.output_dir','/tmp');
-}
-
-// from c9s/php-fileutil
-if ( ! extension_loaded('fileutil') ) {
-    require "FileUtil.php";
 }
