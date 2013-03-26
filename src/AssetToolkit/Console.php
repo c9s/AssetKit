38ee54c69b9f457fcb3e984990c373aa1ca76424
c9s <cornelius.howl@gmail.com>
2013-03-26 12:02:03 +0800

    Checking in changes prior to tagging of version 2.0.16.

--- src/AssetToolkit/Console.php
+++ src/AssetToolkit/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'assetkit';
-    const VERSION = "2.0.15";
+    const VERSION = "2.0.16";
 
     static function getInstance()
     {
