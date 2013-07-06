b887f76d7e4628fb24dac79aff7dc508f05db3e0
c9s <cornelius.howl@gmail.com>
2013-07-06 11:30:35 +0800

    Fix require from phar

--- scripts/assetkit.php
+++ scripts/assetkit.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
-require "FileUtil.php"; // require the FileUtil.php from phar
+// require the FileUtil.php from phar
+require "phar://assetkit.phar/FileUtil.php";
 $app = AssetToolkit\Console::getInstance();
 $app->run( $argv );
