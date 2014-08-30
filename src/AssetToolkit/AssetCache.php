d4feb7c476c66d688afe6b7bc6d1d562a4817351
c9s <yoanlin93@gmail.com>
2014-08-30 23:26:30 +0800

    Implement AssetCache for the backend cache

---
+++ src/AssetToolkit/AssetCache.php
@@ -0,0 +1,157 @@
+<?php
+namespace AssetToolkit;
+use ArrayAccess;
+
+/**
+ * An asset cache container that caches the config of the 
+ * assets.
+ */
+class AssetCache implements ArrayAccess
+{
+    /**
+     * @var string namespace for caching
+     */
+    public $namespace;
+
+    public $options = array();
+
+    /**
+     * @array the assets array that contains the config of all assets.
+     *
+     *   $assets = [
+     *     [asset name] = [ ... ];
+     *   ];
+     */
+    public $assets = array();
+
+    public function __construct($options = array()) {
+        $this->options = $options;
+        if ( isset($options['namespace']) ) {
+            $this->namespace = $options['namespace'];
+        }
+    }
+
+    /**
+     *
+     * Get the config of name asset.
+     *
+     * The asset config contains:
+     *
+     *   "manifest": "tests\/assets\/jquery\/manifest.yml"
+     *   "source_dir": "tests\/assets\/jquery"
+     *   "name": "jquery"
+     *
+     * @param string $name asset name
+     */
+    public function get($name)
+    {
+        if( isset($this->assets[$name]) ) {
+            return $this->assets[$name];
+        }
+    }
+
+    /**
+     * @var string check if we've loaded this asset.
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($this->assets[$name]);
+    }
+
+    /**
+     * Add asset to the config assets.
+     *
+     * @param string $name
+     * @param array $config
+     */
+    public function set($name, $config)
+    {
+        $this->assets[$name] = $config;
+    }
+
+
+    /**
+     * Remove asset from config
+     *
+     * @param string $name asset name
+     */
+    public function remove($name)
+    {
+        unset($this->assets[$name]);
+    }
+
+
+    /**
+     * Add an asset object to the config assets.
+     *
+     * @param Asset $asset
+     */
+    public function add(Asset $asset)
+    {
+        if( ! isset($this->assets[$asset->name]) ) {
+            $this->assetObjects[] = $asset;
+            return $this->assets[$asset->name] = $asset;
+        }
+
+
+        /*
+        XXX:
+        $this->assets[ $asset->name ] = array(
+            'manifest' => $asset->manifestFile,
+            'source_dir' => $asset->sourceDir,
+            'name' => $asset->name,
+        );
+         */
+    }
+
+
+    /**
+     * Returns all asset objects (keys and values)
+     */
+    public function pairs()
+    {
+        return $this->assets;
+    }
+
+    /**
+     * This method returns all assets in an indexed array.
+     *
+     * @return Asset[]
+     */
+    public function all()
+    {
+        return $this->assetObjects;
+    }
+
+
+
+
+    /***********************************************
+     * Methods implemented for ArrayAccess interface.
+     ***********************************************/
+
+
+    public function offsetSet($name, $value)
+    {
+        $this->assets[ $name ] = $value;
+    }
+
+    public function offsetExists($name)
+    {
+        return isset($this->assets[ $name ]);
+    }
+
+    public function offsetGet($name)
+    {
+        return $this->assets[ $name ];
+    }
+
+    public function offsetUnset($name)
+    {
+        unset($this->assets[$name]);
+    }
+
+}
+
+
