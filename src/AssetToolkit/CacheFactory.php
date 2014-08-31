b89e4d475fee30b36eefb989159a6d4df726b3bc
c9s <yoanlin93@gmail.com>
2014-08-31 10:46:17 +0800

    Rename existing Cache to CacheFactory
    Signed-off-by: c9s <yoanlin93@gmail.com>

--- src/AssetToolkit/CacheFactory.php
+++ src/AssetToolkit/CacheFactory.php
@@ -3,14 +3,15 @@ namespace AssetToolkit;
 use UniversalCache\ApcCache;
 use UniversalCache\FileSystemCache;
 use UniversalCache\UniversalCache;
+use AssetToolkit\AssetConfig;
 
-class Cache
+class CacheFactory
 {
 
     /**
      * Create default universal cache from config object.
      */
-    static public function create($config)
+    static public function create(AssetConfig $config)
     {
         $cache = new UniversalCache(array());
 
