4146d4a54d60ab0532ff44eb9f8be244e329218a
c9s <yoanlin93@gmail.com>
2014-09-02 22:45:00 +0800

    install apcu-beta

---
+++ tests/install-apcu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this script is in a `bin/` folder
+
+if [ "$TRAVIS_PHP_VERSION" == "5.3" ]
+then
+    exit 0
+fi
+
+# this is helpful to compile extension
+sudo apt-get install autoconf
+
+# install this version
+APCU=4.0.2
+
+# compile manually, because `pecl install apcu-beta` keep asking questions
+wget http://pecl.php.net/get/apcu-$APCU.tgz
+tar zxvf apcu-$APCU.tgz
+cd "apcu-${APCU}"
+phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
