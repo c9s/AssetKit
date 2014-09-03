8b1e794cf7c27d444550beba6287aa86af32382e
c9s <yoanlin93@gmail.com>
2014-09-03 12:06:43 +0800

    Add shell script for testing asset commands

---
+++ tests/test-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+php bin/assetkit init --baseDir "public/assets" --baseUrl "/assets" assetkit.yml
+cat .assetkit.yml
+php bin/assetkit add tests/assets/jquery
+php bin/assetkit list
+php bin/assetkit remove jquery
+php bin/assetkit target add main jquery
+php bin/assetkit target list
+php bin/assetkit compile jquery
