c47fd9dd31e5c4b8a7c95734f1bc82c9adedb56b
c9s <yoanlin93@gmail.com>
2014-10-07 16:35:24 +0800

    Add CommandTest

---
+++ tests/AssetKit/CommandTest.php
@@ -0,0 +1,48 @@
+<?php
+use CLIFramework\Testing\CommandTestCase;
+
+class CommandTest extends CommandTestCase
+{
+
+    public function setupApplication()
+    {
+        return new AssetKit\Console;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        ob_start();
+        $this->runCommand('assetkit init --baseDir public/assets --baseUrl /assets --dir tests/assets assetkit.yml');
+        ob_end_clean();
+    }
+
+
+    public function testAddAndRemove()
+    {
+        ob_start();
+        $this->runCommand('assetkit add tests/assets/jquery');
+        $this->runCommand('assetkit list');
+        $this->runCommand('assetkit remove jquery');
+        ob_end_clean();
+    }
+
+    public function testCompile()
+    {
+        ob_start();
+        $this->runCommand('assetkit add tests/assets/jquery');
+        $this->runCommand('assetkit add tests/assets/underscore');
+        $this->runCommand('assetkit add tests/assets/webtoolkit');
+        $this->runCommand('assetkit add tests/assets/jquery-ui');
+        $this->runCommand('assetkit compile jquery');
+        ob_end_clean(); // XXX: looks like compile command didn't close its own buffer.
+
+        $this->runCommand('assetkit compile --target all jquery underscore webtoolkit jquery-ui');
+        ob_end_clean(); // XXX: looks like compile command didn't close its own buffer.
+
+        ob_end_clean();
+    }
+}
+
+
+
