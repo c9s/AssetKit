a29bbbefd82a22b6dd9aa0108cafac70e0168acd
c9s <yoanlin93@gmail.com>
2014-10-15 22:08:47 +0800

    More create-manifest command improvements

--- src/AssetKit/Command/CreateManifestCommand.php
+++ src/AssetKit/Command/CreateManifestCommand.php
@@ -9,6 +9,8 @@ use AssetKit\LinkInstaller;
 use AssetKit\Command\BaseCommand;
 use CLIFramework\Command;
 use Exception;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 use ConfigKit\ConfigCompiler;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Dumper;
@@ -17,11 +19,15 @@ class CreateManifestCommand extends Command
 {
     public function brief() { return 'Create asset manifest file'; }
 
-    public function execute($path = NULL)
+    public function options($opts) {
+        $opts->add('d|dir+', 'Possible relative paths to the manifest file that contains asset files.');
+    }
+
+
+    public function execute($manifestDir = NULL)
     {
-        if ($path) {
-            chdir($path);
-        }
+        $manifestDir = $manifestDir ?: getcwd();
+        $possiblePaths = $this->options->dir ?: array();
 
         $jsFiles = array();
         $cssFiles = array();
@@ -36,33 +42,83 @@ class CreateManifestCommand extends Command
 
         $config = array();
         $config['collections'] = array();
-        foreach($inspectDirs as $type => $dirs) {
-            foreach($dirs as $dir) {
-                if (file_exists($dir)) {
-                    $this->logger->info("Found $dir as type $type.");
-                    $config['collections'][] = array( $type => array($dir) );
-                    break;
+
+
+        $files = array(
+            'stylesheet' => array(),
+            'javascript' => array(),
+            'file' => array(),
+        );
+        $patterns = array(
+            'stylesheet' => '/\.css$/',
+            'javascript' => '/\.js/',
+            'file' => '/\.(?:png|jpe?g|ico|gif|eot|svg|ttf|woff)$/',
+        );
+
+        if (empty($possiblePaths)) {
+
+            foreach($inspectDirs as $type => $dirs) {
+                foreach($dirs as $dir) {
+                    if (file_exists($manifestDir . DIRECTORY_SEPARATOR . $dir)) {
+                        $this->logger->info("Found $dir as type $type.");
+                        $config['collections'][] = array( $type => array($dir) );
+                    }
+                }
+            }
+
+            $di = new \RecursiveDirectoryIterator($manifestDir, RecursiveDirectoryIterator::SKIP_DOTS);
+            $it = new \RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
+            foreach($it as $info) {
+                if ($info->isFile()) {
+                    $path = $it->getSubPathname();
+                    foreach($patterns as $type => $pattern) {
+                        if (preg_match($pattern, $path)) {
+                            $this->logger->info("Adding $path as $type");
+                            $files[$type][] = $path;
+                        }
+                    }
+                }
+            }
+
+        } else {
+            // Search recursively
+            foreach($possiblePaths as $path) {
+                $searchPath = $manifestDir . DIRECTORY_SEPARATOR . $path;
+                if (!file_exists($searchPath)) {
+                    $this->logger->warn("$searchPath does not exist.");
+                    continue;
+                }
+
+                $di = new \RecursiveDirectoryIterator($searchPath, RecursiveDirectoryIterator::SKIP_DOTS);
+                $it = new \RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
+                foreach($it as $info) {
+                    if ($info->isFile()) {
+                        $item = $path . DIRECTORY_SEPARATOR . $it->getSubPathname();
+                        foreach($patterns as $type => $pattern) {
+                            if (preg_match($pattern, $item)) {
+                                $this->logger->info("Adding $item as $type");
+                                $files[$type][] = $item;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        $jsFiles = glob("*.js");
-        $jsFiles = array_filter($jsFiles, function($file) {
-            return ! preg_match('/\.min\.js/',$file);
-        });
-        if (!empty($jsFiles)) {
-            $config['collections'][] = array('javascript' => $jsFiles);
-        }
 
-        $cssFiles = glob("*.css");
-        $cssFiles = array_filter($cssFiles, function($file) {
-            return ! preg_match('/\.min\.css$/',$file);
+        // Remove minified files
+        $files['javascript'] = array_filter($files['javascript'], function($file) {
+            return !preg_match('/[.-](?:min|pack)\.js$/',$file);
         });
-
-        if (!empty($cssFiles)) {
-            $config['collections'][] = array('stylesheet' => $cssFiles);
+        $files['stylesheet'] = array_filter($files['stylesheet'], function($file) {
+            return !preg_match('/[.-](?:min|pack)\.css$/',$file);
+        });
+        foreach($files as $type => $files) {
+            if (!empty($files)) {
+                $config['collections'][] = array($type => array_values($files));
+            }
         }
-        ConfigCompiler::write_yaml('manifest.yml', $config);
+        ConfigCompiler::write_yaml($manifestDir . DIRECTORY_SEPARATOR . 'manifest.yml', $config);
         $this->logger->info("Done");
     }
 }
