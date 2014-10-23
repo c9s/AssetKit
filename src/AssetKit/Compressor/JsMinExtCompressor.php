40cd79bca7903b56331dac9c5c4731f4fccf1be0
c9s <yoanlin93@gmail.com>
2014-10-23 16:12:10 +0800

    Add JsMinExtCompressor class

---
+++ src/AssetKit/Compressor/JsMinExtCompressor.php
@@ -0,0 +1,37 @@
+<?php
+namespace AssetKit\Compressor;
+use AssetKit\Collection;
+use AssetKit\Process;
+use AssetKit\JSMin;
+use RuntimeException;
+use Exception;
+
+class JsMinExtException extends Exception {  }
+
+class JsMinExtCompressor
+{
+    public $messages = array(
+        JSMIN_ERROR_UNTERMINATED_STRING => 'Unterminated string.',
+        JSMIN_ERROR_UNTERMINATED_COMMENT => 'Unterminated comment.',
+        JSMIN_ERROR_UNTERMINATED_REGEX => 'Unterminated regex.',
+    );
+
+    public function compress(Collection $collection)
+    {
+        // C version jsmin is faster,
+        $content = $collection->getContent();
+        $content = jsmin($content);
+        $err = jsmin_last_error_msg();
+        if ($err != JSMIN_ERROR_NONE) {
+            if (isset($this->messages[$err])) {
+                throw new Exception($this->messages[$err]);
+            } else {
+                throw new Exception("Unknown Error");
+            }
+        }
+        $collection->setContent($content);
+    }
+}
+
+
+
