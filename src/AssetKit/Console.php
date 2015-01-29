6a0ca169b0de77596a6252089707b69051435f73
c9s <yoanlin93@gmail.com>
2015-01-30 02:19:00 +0800

    Checking in changes prior to tagging of version 3.0.11.

--- src/AssetKit/Console.php
+++ src/AssetKit/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'assetkit';
-    const VERSION = "3.0.10";
+    const VERSION = "3.0.11";
 
     static function getInstance()
     {
