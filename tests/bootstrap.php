be87c11f481309ef461e696455a4f613d04cf8eb
c9s <yoanlin93@gmail.com>
2015-08-04 01:35:24 +0800

    Remove compiled cached before running tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,16 @@
 <?php
 $autoloader = require 'vendor/autoload.php';
 
-if( extension_loaded('apc') ) {
+if (extension_loaded('apc')) {
     apc_clear_cache();
 }
 if (extension_loaded('xhprof') ) {
     ini_set('xhprof.output_dir','/tmp');
 }
+
+if (file_exists('tests/public/compiled')) {
+    futil_rmtree('tests/public/compiled');
+    mkdir('tests/public/compiled', 0755, true);
+}
+
 return $autoloader;
