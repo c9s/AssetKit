e3b80f5dbd994639b11bcc2902376e4f926f9fe7
c9s <yoanlin93@gmail.com>
2016-03-27 23:23:45 +0800

    apcu 5.1.3 is for php7

--- tests/install-apcu.sh
+++ tests/install-apcu.sh
@@ -11,10 +11,12 @@ fi
 sudo apt-get install autoconf
 
 # install this version
-APCU=5.1.3
+APCU_VERSION=4.0.10
+
+# 5.1.3 is for php7
 
 # compile manually, because `pecl install apcu-beta` keep asking questions
-wget http://pecl.php.net/get/apcu-$APCU.tgz
-tar zxvf apcu-$APCU.tgz
-cd "apcu-${APCU}"
-phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
+wget http://pecl.php.net/get/apcu-$APCU_VERSION.tgz
+tar zxvf apcu-$APCU_VERSION.tgz
+cd "apcu-${APCU_VERSION}"
+phpize && ./configure && make install && echo "Installed ext/apcu-${APCU_VERSION}"
