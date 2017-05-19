d5fa11c19f2b46712df1850e5e5d6e31211c9799
c9s <yoanlin93@gmail.com>
2017-05-19 22:13:57 +0800

    Fix apcu installer

--- tests/travis/install-apcu.sh
+++ tests/travis/install-apcu.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
-
-# this script is in a `bin/` folder
-
-if [ "$TRAVIS_PHP_VERSION" == "5.3" ]
-then
-    exit 0
+if [[ ${TRAVIS_PHP_VERSION:0:1} == "7" ]] ; then
+    APCU_VERSION=5.1.8
+else
+    APCU_VERSION=4.0.11
 fi
-
-# this is helpful to compile extension
-sudo apt-get install autoconf
-
-# install this version
-APCU_VERSION=4.0.10
-
-if [ "$TRAVIS_PHP_VERSION" == "7.0" ]; then
-    # 5.1.3 is for php7
-    APCU_VERSION=5.1.3
-fi
+PACKAGE=apcu-$APCU_VERSION
 
 # compile manually, because `pecl install apcu-beta` keep asking questions
-wget http://pecl.php.net/get/apcu-$APCU_VERSION.tgz
-tar zxvf apcu-$APCU_VERSION.tgz
-cd "apcu-${APCU_VERSION}"
-phpize && ./configure && make install && echo "Installed ext/apcu-${APCU_VERSION}"
+wget http://pecl.php.net/get/$PACKAGE.tgz
+tar zxvf $PACKAGE.tgz
+(cd $PACKAGE && phpize && ./configure && make install && echo "Installed ext/$PACKAGE")
