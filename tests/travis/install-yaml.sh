7f317c113c044b5e956e257a77ca38745b53d2ff
c9s <yoanlin93@gmail.com>
2017-05-19 22:26:40 +0800

    check if yaml ext dir exists

--- tests/travis/install-yaml.sh
+++ tests/travis/install-yaml.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
+mkdir exts
+cd exts
 if [[ ${TRAVIS_PHP_VERSION:0:1} == "7" ]] ; then
     PACKAGE=yaml-2.0.0
 else
     PACKAGE=yaml-1.3.0
 fi
-wget -c http://pecl.php.net/get/$PACKAGE.tgz
-tar -xzf $PACKAGE.tgz
+if [[ ! -e $PACKAGE ]] ; then
+    wget -c http://pecl.php.net/get/$PACKAGE.tgz
+    tar xzf $PACKAGE.tgz
+fi
 (cd $PACKAGE && phpize && ./configure && make && sudo make install)
 echo "extension=yaml.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
